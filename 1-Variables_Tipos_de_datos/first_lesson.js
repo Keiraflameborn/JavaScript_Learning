3cbfc2aa438e1e27d020cfa335a933afc6b0b100
Isa <isabellagry@gmail.com>
2024-12-02 11:34:53 -0500

    data types

---
+++ 1-Variables_Tipos_de_datos/first_lesson.js
@@ -0,0 +1,42 @@
+/* Tipos de datos 
+Las variables se pueden declarar de dos formas, utilizando la palabra
+reservada let o var, asignarle un nombre y opcional un valor.
+El simbolo = es para asignación.
+El punto y coma al final denotan la finalización del bloque código
+Tipos de datos:
+-String: hace referencia a una cadena de texto: aplica javascrip
+*/
+
+let numero = 5; //number
+console.log(numero)
+let texto = 'Este es un texto de prueba';//string
+let flag = false //boolean
+let bed = undefined; //undefined
+
+numero = 145 //Cambiamos el valor de la variable numero
+
+//Imprime el valor de la variable
+console.log(numero)
+
+//El valor de la constante no puede ser re asignado
+const newNumber = 5
+
+console.log(newNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
