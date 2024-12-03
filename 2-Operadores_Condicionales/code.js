15792050dd4d61298c25f88cf7ab23fe395f240b
Isa <isabellagry@gmail.com>
2024-12-03 17:48:23 -0500

    condicionales

---
+++ 2-Operadores_Condicionales/code.js
@@ -0,0 +1,102 @@
+/**
+ * Operadores lógicos: == + - * != < > % &&: y ||: o
+ * condicionales: if else
+ * 
+ */
+/*
+let animal = "gato"
+
+//Condicional o
+if(animal == 'perro' || animal == 'gato'){
+    console.log("Agotado")
+}
+
+//Condicional y
+
+let number = 5
+if(number >= 5 && number <= 10){
+    console.log('Esta en el rango')
+}
+
+
+//Operador diferente !=
+
+let criatura = 'gato'
+
+if(criatura != 'perro'){
+    //Template string
+    console.log(`El animal ${criatura} no es perro`)
+}
+
+
+
+let numberA = 8;
+let numberB = 6;
+
+if(numberA > numberB){
+    console.log( `El resultado de la resta es ${numberA - numberB}`)
+}
+else if(numberA > 5){
+    console.log( `El resultado de la resta es ${numberA * numberB}`)
+}
+
+else{
+    console.log(`La suma es ${numberA + numberB}`)
+}
+
+let texto = 5;
+let numerico = 5;
+//Comparador estricto en ===
+if(texto === numerico){
+    console.log('son iguales')
+}
+
+
+//formulario con nombre correo telefono
+//Si el formulario esta completo debe imprimir el mensaje ok de lo contrario debe decir que está incompleto
+
+let nombre = ""
+let correo = "@gmail"
+let telefono = "458"
+
+if(nombre !== "" && correo !== "" && telefono !== ""){
+    console.log("Esta completo")
+}
+
+if(nombre === ""){
+    console.log("falta nombre")
+}
+
+if(correo === ""){
+    console.log("falta correo")
+}
+
+if(telefono === ""){
+    console.log("falta telefono")
+}
+*/
+
+let valor1 = 15;
+let valor2 = 10;
+
+
+
+if(valor1 >= 1 && valor2 <= 50){
+   let resultado = valor1 * valor2
+   if(resultado > 100){
+    console.log("es mayor que 100")
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
